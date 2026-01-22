bf90edb966498d551ac440c8bca348f94ea75c03
Lindokuhle <yendelindokuhle2@gmail.com>
2026-01-22 23:41:11 +0200

    Added microservice for room booking, food order and auth

---
+++ RoomBookingService/src/main/java/com/hotelmanagementsystem/roombookingservice/RoomBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmanagementsystem.roombookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomBookingServiceApplication.class, args);
+    }
+
+}
---
+++ RoomBookingService/src/test/java/com/hotelmanagementsystem/roombookingservice/RoomBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelmanagementsystem.roombookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
