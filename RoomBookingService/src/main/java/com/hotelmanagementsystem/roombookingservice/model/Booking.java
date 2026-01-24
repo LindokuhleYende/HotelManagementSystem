9d1a168a353698f46ff8391667a0892c5ff0a82d
Lindokuhle <yendelindokuhle2@gmail.com>
2026-01-24 23:59:30 +0200

    updated entity to fit h2 console requirements

---
+++ RoomBookingService/src/main/java/com/hotelmanagementsystem/roombookingservice/model/Booking.java
@@ -0,0 +1,36 @@
+package com.hotelmanagementsystem.roombookingservice.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import lombok.*;
+
+@Entity
+@Table(name = "booking")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String booking_number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(nullable = false)
+    private LocalDate check_in_date;
+
+    @Column(nullable = false)
+    private LocalDate check_out_date;
+
+    @Column
+    private String availability_status;
+
+    @Column(nullable = false)
+    private String num_of_guests;
+}
