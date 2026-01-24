656cb9d4938abfe6ff0e4f4c310e4e010abd80d5
Lindokuhle <yendelindokuhle2@gmail.com>
2026-01-25 00:03:06 +0200

    updated main application file in RoomBookingServiceApplication

--- RoomBookingService/src/main/java/com/hotelmanagementsystem/roombookingservice/RoomBookingServiceApplication.java
+++ RoomBookingService/src/main/java/com/hotelmanagementsystem/roombookingservice/RoomBookingServiceApplication.java
@@ -2,8 +2,12 @@ package com.hotelmanagementsystem.roombookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.hotelmanagementsystem")
+@EnableJpaRepositories("com.hotelmanagementsystem")
 public class RoomBookingServiceApplication {
 
     public static void main(String[] args) {
