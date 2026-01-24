d5d164db6c0b8d0f7e68c4e682aea22c2db39fc0
Lindokuhle <yendelindokuhle2@gmail.com>
2026-01-25 00:56:26 +0200

    Created a menu entity in a food order service

---
+++ FoodOrderService/src/main/java/com/hotelmanagementsystem/foodorderservice/entity/Menu.java
@@ -0,0 +1,26 @@
+package com.hotelmanagementsystem.foodorderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    public Double price;
+
+    @Column
+    private String category;
+
+    @Column
+    private String images;
+}
