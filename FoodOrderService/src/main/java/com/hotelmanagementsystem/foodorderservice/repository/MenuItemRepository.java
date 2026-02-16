6a4ecb32bf3949d869a3b2d05bdeaaac455ee3a0
cadee247 <rousseaucadee@gmail.com>
2026-02-16 10:32:25 +0200

    Add full HotelManagementSystem project including FoodOrderService and other modules

--- FoodOrderService/src/main/java/com/hotelmanagementsystem/foodorderservice/repository/MenuItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hotelmanagementsystem.foodorderservice.repository;
-
-import com.hotelmanagementsystem.foodorderservice.entity.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {}
